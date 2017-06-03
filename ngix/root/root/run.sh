8b76d4804e5582a38016fb54047bfe6233be9fda
lavender-flowerdew <lavender.flowerdew@users.noreply.github.com>
2017-06-03 10:45:46 +0100

    Now builds configuration at start using CLUSTER_CONF environment variable in form of test~test~172.17.0.2:9000;test2~test2~172.17.0.4:9000

--- ngix/root/root/run.sh
+++ ngix/root/root/run.sh
@@ -3,13 +3,52 @@
 set -e
 
 if [ $1 == "start" ]; then
-  f=/etc/nginx/custom/cluster.conf
-  echo "Creating addtional configuration at"
-  cat <<EOT >> $f
-$CLUSTER_CONF
+  f=/etc/nginx/custom
+  echo "Creating addtional configuration..."
+  echo "$CLUSTER_CONF"
+
+  l=$f/locations.conf
+  s=$f/servers.conf
+  ss=./servers-conf
+
+  IN="$CLUSTER_CONF"
+
+  locations=$(echo $IN | tr ";" "\n")
+
+  for details in $locations
+  do
+      IFS='~' read group location hosts  <<< "$details"
+      cat <<EOT >> $l
+location ~ /$location/([^/]+) {
+  gzip off;
+  include /etc/nginx/common/fastcgi_params;
+  fastcgi_pass $group;
+}
 EOT
+  done
+
+  for details in $locations
+  do
+      IFS='~' read group location hs  <<< "$details"
+      hosts=$(echo $hs | tr "," "\n")
+      for host in $hosts
+      do
+          line=$(echo $host | tr "-" " ")
+          echo "    server $line;" >> $ss
+      done
+
+      servers=`cat $ss`
+      rm -f $ss
+      cat <<EOT >> $s
+upstream $group {
+    $servers
+}
+EOT
+  done
+
   echo "Additional conf is:"
-  cat $f
+  cat $l
+  cat $s
   shift 1
   echo "Starting nginx"
   (nginx)
